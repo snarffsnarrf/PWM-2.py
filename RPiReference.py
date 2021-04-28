6a4ea0514a22e66c8fd239e4b21d75037ea43f0f
editingbay <desmondafoster@gmail.com>
2021-04-28 01:42:20 -0400

    0.1.1? Really tho

--- RPiReference.py
+++ RPiReference.py
@@ -1,15 +1,22 @@
 # To create a PWM instance:
 # p = GPIO.PWM(channel, frequency)
+#
 # To start PWM:
 # p.start(dc)   # where dc is the duty cycle (0.0 <= dc <= 100.0)
+#
 # To change the frequency:
 # p.ChangeFrequency(freq)   # where freq is the new frequency in Hz
+#
 # To change the duty cycle:
 # p.ChangeDutyCycle(dc)  # where 0.0 <= dc <= 100.0
+#
 # To stop PWM:
 # p.stop()
-# Note that PWM will also stop if the instance variable 'p' goes out of scope.
+#
+#    # Note that PWM will also stop if the instance variable 'p' goes out of scope.
+#
 # An example to blink an LED once every two seconds:
+#
 # import RPi.GPIO as GPIO
 # GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(12, GPIO.OUT)
@@ -19,6 +26,7 @@
 # input('Press return to stop:')   # use raw_input for Python 2
 # p.stop()
 # GPIO.cleanup()
+#
 # An example to brighten/dim an LED:
 # import time
 # import RPi.GPIO as GPIO

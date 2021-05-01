d9f329b356b715b701815c438c0954d31fb884da
pi <desmondafoster@gmail.com>
2021-04-30 23:39:38 -0400

    0.1.2 Raspi
    Got the classes to successfully call into the GPIO I think.
    WHO DO YOU THINK YOU ARE, I AM!

--- classtree.py
+++ classtree.py
@@ -1,50 +1,47 @@
-# import RPi.GPIO as GPIO
-import time
+
 
 Hfreq = 100
 Lfreq = 50
-Tpin = 11
-Bpin = 13
-
+toppin = 11
+botpin = 13
 
 
 class Shot:
 
-    def __init__(self, tfreq, bfreq, tpin, bpin, tduty, bduty):
+    def __init__(self, tfreq, bfreq, toppin, botpin, tduty, bduty, name):
         self.tfreq = tfreq
         self.bfreq = bfreq
-        self.tpin = tpin
-        self.bpin = bpin
+        self.tpin = toppin
+        self.bpin = botpin
         self.tduty = tduty
         self.bduty = bduty
+        self.name = name
 
     def topspin(self):
         self.tfreq = Hfreq
         self.bfreq = Hfreq
-        self.tpin = Tpin
-        self.bpin = Bpin
+        self.tpin = toppin
+        self.bpin = botpin
         self.tduty = 100
         self.bduty = 80
+        self.name = "Topspin"
 
     def backspin(self):
         self.tfreq = Hfreq
         self.bfreq = Hfreq
-        self.tpin = Tpin
-        self.bpin = Bpin
+        self.tpin = toppin
+        self.bpin = botpin
         self.tduty = 80
         self.bduty = 100
+        self.name = "Backspin"
 
 
-def Shottype(self):
-    self.topspin = Shot.topspin()
+# def Shottype(self):
+#     self.topspin = Shot.topspin()
 
 
-
-
-
-Shot = Shot(0, 0, 0, 0, 0, 0)
+Shot = Shot(1, 1, 0, 0, 0, 0, 0)
 
 def output():
     return 'Top Freq={} | Bot Freq={} | Top Pin={} | Bot Pin={} | TDuty {} | Bduty {}'\
         .format(Shot.tfreq, Shot.bfreq, Shot.tpin, Shot.bpin, Shot.tduty, Shot.bduty)
-
--- logicTest.py
+++ logicTest.py
@@ -4,29 +4,32 @@ import RPi.GPIO as GPIO
 
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(Shot.tpin, GPIO.OUT)    # Enable pin top
-GPIO.setup(Shot.bpin, GPIO.OUT)    # Enable pin Bottom
+GPIO.setup(11, GPIO.OUT)    # Enable pin top
+GPIO.setup(13, GPIO.OUT)    # Enable pin Bottom
 GPIO.setup(16, GPIO.OUT)    # direction control t.1
 GPIO.setup(18, GPIO.OUT)    # direction control t.2
 GPIO.setup(29, GPIO.OUT)    # direction control b.1
 GPIO.setup(31, GPIO.OUT)    # direction control b.2
 
-t = GPIO.PWM(Shot.tpin, Shot.tfreq)
-b = GPIO.PWM(Shot.bpin, Shot.bfreq)
-Shot = Shot(0, 0, 0, 0, 0, 0)
+t = GPIO.PWM(11, Shot.tfreq)
+b = GPIO.PWM(13, Shot.bfreq)
+
 
 
 Shot.topspin()
 while True:
-    t.ChangeDutyCycle(Shot.tduty())
-    b.ChangeDutyCycle(Shot.bduty())
-    print('{} - {}'.format(Shot.tduty, Shot.bduty))
+    t.ChangeDutyCycle(Shot.tduty)
+    b.ChangeDutyCycle(Shot.bduty)
+    print('Top Duty = {} | Bottom Duty = {} | Name = {} |'.format(Shot.tduty, Shot.bduty, Shot.name))
     break
 time.sleep(5)
 Shot.backspin()
 while True:
-    t.ChangeDutyCycle(Shot.bduty())
-    b.ChangeDutyCycle(Shot.bduty())
-    print('{} - {}'.format(Shot.tduty, Shot.bduty))
+    t.ChangeDutyCycle(Shot.bduty)
+    b.ChangeDutyCycle(Shot.bduty)
+    print('Top Duty = {} | Bottom Duty = {} | Name = {} |'.format(Shot.tduty, Shot.bduty, Shot.name))
     break
+GPIO.cleanup()
 
+
+# Shot = Shot(1, 1, 0, 0, 0, 0)
